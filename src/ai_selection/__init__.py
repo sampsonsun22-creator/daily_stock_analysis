1b500e86b0e57f2824f96d98df4516fc932c39c7
sampsonsun22-creator <sampsonsun22@gmail.com>
2026-07-15 15:55:03 +0800

    feat: export multi-source screeners

--- src/ai_selection/__init__.py
+++ src/ai_selection/__init__.py
@@ -2,15 +2,22 @@
 
 from .models import PrefilterCandidate, RankedSelection, SelectionRun
 from .scoring import SelectionScorer
-from .screening import VibeMarketScreener, build_candidate_pool
+from .screening import (
+    CompositeMarketScreener,
+    TushareEodScreener,
+    VibeMarketScreener,
+    build_candidate_pool,
+)
 from .service import AISelectionService
 
 __all__ = [
     "AISelectionService",
+    "CompositeMarketScreener",
     "PrefilterCandidate",
     "RankedSelection",
     "SelectionRun",
     "SelectionScorer",
+    "TushareEodScreener",
     "VibeMarketScreener",
     "build_candidate_pool",
 ]
